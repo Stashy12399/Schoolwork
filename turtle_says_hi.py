04e0d078a0a7e0788f0d07e36d015698df7d90d3
Stashy12399 <91324567+Stashy12399@users.noreply.github.com>
2021-09-24 13:08:24 +0100

    Create turtle_says_hi.py

---
+++ turtle_says_hi.py
@@ -0,0 +1,33 @@
+import turtle
+# modules that are needed
+
+t = turtle.Turtle()
+# lets me type "t." instead of "turtle." which saves a lot of time
+t.left(90)
+t.forward(80)
+t.backward(40)
+t.right(90)
+t.forward(50)
+t.left(90)
+t.forward(40)
+t.backward(80)
+t.color('white')
+t.right(90)
+t.forward(50)
+t.color('black')
+t.left(90)
+t.forward(80)
+t.left(90)
+t.forward(25)
+t.backward(50)
+t.forward(25)
+t.left(90)
+t.forward(80)
+t.left(90)
+t.forward(25)
+t.backward(50)
+t.color('white')
+t.backward(50)
+# all this draws "hi, made when i first learned how to become a turtle godddd
+
+
