983abbf084f20e77e5883e62cc3728eda51fed3b
Stasiu <91324567+Stashy12399@users.noreply.github.com>
2021-09-30 17:54:58 +0100

    Create if_else.py

---
+++ if_else.py
@@ -0,0 +1,9 @@
+num = 0
+for i in range(1, 51):
+    num = num + 1
+    if num % 2 == 0:
+        print(num, " is an even number")
+    else:
+        print(num, "is an odd number")
+# loop of numbers from 1 to 50, if the number is an even, it tells you its an even, if its odd, it tells you if its odd
+# this was homework
